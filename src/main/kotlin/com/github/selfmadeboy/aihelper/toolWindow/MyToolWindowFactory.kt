37710d3530c27a6aebf054ff34fc548c0d02520e
GitHub Action <action@github.com>
2024-01-05 05:31:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/selfmadeboy/aihelper/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/selfmadeboy/aihelper/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.selfmadeboy.aihelper.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.selfmadeboy.aihelper.MyBundle
+import com.github.selfmadeboy.aihelper.services.MyProjectService
 import javax.swing.JButton
 
 
